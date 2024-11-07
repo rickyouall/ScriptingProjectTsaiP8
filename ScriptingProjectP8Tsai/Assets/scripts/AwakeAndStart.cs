6f3fd5b727e2d98d1f5011acbd29ec384b2ab0df
rickyouall <740502@student.dusd.net>
2024-11-07 14:15:07 -0800

    completeing the awake and start
    today i ha d finished the scripting assignment and it's the easiest from all of the scripting assignment.

---
+++ ScriptingProjectP8Tsai/Assets/scripts/AwakeAndStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
