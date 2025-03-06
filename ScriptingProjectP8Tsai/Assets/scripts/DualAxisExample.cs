a89d49c6b1d16b3bea6b26a217c4f7a1485207d4
rickyouall <740502@student.dusd.net>
2025-03-06 15:30:07 -0800

    I finihsed the new  assignment.
    the new assignmnet

---
+++ ScriptingProjectP8Tsai/Assets/scripts/DualAxisExample.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine;
+public class DualAxisExample : MonoBehaviour
+{
+    public Text horizontalValueDisplayText;
+    public Text verticalValueDisplayText;
+    public float hRange;
+    public float vRange;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float xPos = h * hRange;
+        float yPos = v * vRange;
+
+        transform.position = new Vector3(xPos, 0,yPos);
+        horizontalValueDisplayText.text = h.ToString("F2");
+        verticalValueDisplayText.text = v.ToString("F2");
+    }
+}
