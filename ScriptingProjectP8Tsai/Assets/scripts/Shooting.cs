8c4163e98f2723574cc9ba265a86e7fcba5a6e22
rickyouall <740502@student.dusd.net>
2025-04-02 15:01:17 -0700

    making a tank game
    I made a tank and it shotts

---
+++ ScriptingProjectP8Tsai/Assets/scripts/Shooting.cs
@@ -0,0 +1,45 @@
+using JetBrains.Rider.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+
+
+    private Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+
+
+    void Awake()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && inventory.myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            inventory.myStuff.bullets--;
+        }
+    }
+}
